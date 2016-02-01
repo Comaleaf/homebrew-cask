26e183c035a5ad60ae021a087b7749a2de9bc3f1
Lauren <lauren@tomasello.me>
2016-02-01 15:04:53 +0000

    updated mnemosyne (2.3.5)

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -1,9 +1,9 @@
 cask 'mnemosyne' do
-  version '2.3'
-  sha256 '094c4f6fb50de376a5190c3712b935089579717641ce90685aa48932bf0efa07'
+  version '2.3.5'
+  sha256 '88c593fd7cdf2ceb42bf1af9977765b7a233a42185181ae873b960cf644211b7'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/sourceforge/mnemosyne-proj/Mnemosyne-#{version}-Mac.dmg"
+  url "http://downloads.sourceforge.net/project/mnemosyne-proj/mnemosyne/mnemosyne-#{version}/Mnemosyne-#{version}.dmg"
   name 'Mnemosyne'
   homepage 'http://mnemosyne-proj.org/'
   license :gpl
